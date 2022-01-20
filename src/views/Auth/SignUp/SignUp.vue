41453739495518c99df55ef7eb8a16ab289eed31
melisovm97 <marsel10081997>
2022-01-20 10:21:32 +0600

    test commit from vs code

--- src/views/Auth/SignUp/SignUp.vue
+++ src/views/Auth/SignUp/SignUp.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    {{ step }}
-    <component :is="currentComponent" @step="onStep" />
+    <component
+      :is="currentComponent"
+      @step="onStep"
+    />
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang='ts' setup>
 import { useAuthStore } from '@/stores/auth';
 import { SignUp1Step, SignUp2Step } from '@/views/Auth/SignUp/index';
 import { computed } from 'vue-demi';
@@ -35,7 +37,7 @@ function onStep() {
 }
 </script>
 
-<style lang="scss">
+<style lang='scss'>
 .sign-up--container {
   padding: 0 16px;
 
--- src/views/Auth/SignUp/SignUp1Step.vue
+++ src/views/Auth/SignUp/SignUp1Step.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="sign-up--container">
-    <TopNavigation>
-      Enter phone number
-    </TopNavigation>
+    <TopNavigation> Enter phone number </TopNavigation>
     <div class="sign-up--description text--body">
       We will send verification code
-      <br>
+      <br />
       to this number.
     </div>
     <div class="grid">
@@ -13,71 +11,63 @@
         <BaseCountryPhoneInput />
       </div>
       <div class="col-8 ml-auto">
-        <BaseInput
-          v-model="number"
-          :use-grouping="false"
-          type="number"
-        >
-          <template #label>
-            Number
-          </template>
+        <BaseInput v-model="number" :use-grouping="false" type="number">
+          <template #label> Number </template>
         </BaseInput>
       </div>
     </div>
     <div class="footer">
       <span class="text--footnote font-weight--semibold">
         Already have an account?
-        <span class="link">
-          Log in
-        </span>
+        <span class="link"> Log in </span>
       </span>
     </div>
     <div class="sign-button-wrapper">
-      <BaseButton
-        block
-        @click="nextStep"
-      >
-        Sign up
-      </BaseButton>
+      <BaseButton block @click="nextStep"> Sign up </BaseButton>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { defineEmits, ref } from "vue";
+import TopNavigation from "@/components/UI/TopNavigation.vue";
+import BaseInput from "@/components/UI/BaseInput.vue";
+import BaseCountryPhoneInput from "@/components/UI/BaseCountryPhoneInput.vue";
+import BaseButton from "@/components/UI/BaseButton.vue";
 
-import { defineComponent, ref } from 'vue';
-import TopNavigation from '@/components/UI/TopNavigation.vue';
-import BaseInput from '@/components/UI/BaseInput.vue';
-import BaseCountryPhoneInput from '@/components/UI/BaseCountryPhoneInput.vue';
-import BaseButton from '@/components/UI/BaseButton.vue';
+import { StepDirection } from "@/views/Auth/SignUp/types";
 
-import { StepDirection } from '@/views/Auth/SignUp/types';
+// export default defineComponent({
+//   name: "SignUp",
+//   components: {
+//     TopNavigation,
+//     BaseInput,
+//     BaseCountryPhoneInput,
+//     BaseButton,
+//   },
+//   emits: ["step"],
+//   setup() {
+//     const number = ref(null);
 
-export default defineComponent({
-  name: 'SignUp',
-  components: {
-    TopNavigation,
-    BaseInput,
-    BaseCountryPhoneInput,
-    BaseButton
-  },
-emits: ['step'],
-  setup() {
-    const number = ref(null);
+//     return {
+//       number,
+//     };
+//   },
 
-    return {
-      number
-    };
-  },
+//   methods: {
+//     nextStep() {
+//       this.$emit("step", StepDirection.next);
+//     },
+//   },
+// });
 
-  methods: {
-    nextStep() {
-      this.$emit('step', StepDirection.next)
-    }
-  }
-});
+const number = ref(null);
+
+const emit = defineEmits(["step"]);
+
+function nextStep(): void {
+  emit("step", StepDirection.next);
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
