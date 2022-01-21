1c96366a8f6c0f0ea1840d90fa51555b38da32a5
Shutemov <shutemov1997@mail.ru>
2022-01-21 13:08:12 +0300

    Implements the WelcomeAuthScreen template

--- src/views/Welcome/WelcomeAuthScreen.vue
+++ src/views/Welcome/WelcomeAuthScreen.vue
@@ -1,4 +1,16 @@
 <template>
+  <div class="welcome-auth">
+    <nav class="slider"></nav>
+    <header class="header">
+      <h1 class="title">Welcome to</h1>
+      <img class="logo" src="@/assets/images/full-logo-white.png" alt="logo" />
+    </header>
+    <section class="content"></section>
+    <footer class="footer">
+      <button class="login">Log in</button>
+      <button class="signup">Sign up</button>
+    </footer>
+  </div>
 </template>
 
 <script setup lang="ts">
