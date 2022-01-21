2c18163c69e634e436307ba6b3e052205d73f4cf
Shutemov <shutemov1997@mail.ru>
2022-01-21 17:07:51 +0300

    Create StoresSlider.vue

---
+++ src/components/UI/StoresSlider.vue
@@ -0,0 +1,26 @@
+<template>
+  <vue-agile :infinite="true" :autoplay-speed="2000">
+    <div v-for="({ link }, index) in stories" :key="index">
+      <img :src="link" />
+    </div>
+    <template v-slot:prev><button>prev</button></template>
+  </vue-agile>
+</template>
+
+<script setup lang="ts">
+import { VueAgile } from 'vue-agile';
+
+interface Story {
+  link: string;
+}
+
+interface Props {
+  stories?: Story[];
+  timeInterval?: number;
+}
+
+defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+</style>
