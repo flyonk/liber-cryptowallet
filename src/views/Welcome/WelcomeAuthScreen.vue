78a0e367b1dd01bd30f18e1692ffdf758d1e4e60
Shutemov <shutemov1997@mail.ru>
2022-01-21 13:08:49 +0300

    Implements the WelcomeAuthScreen styles

--- src/views/Welcome/WelcomeAuthScreen.vue
+++ src/views/Welcome/WelcomeAuthScreen.vue
@@ -17,4 +17,54 @@
 </script>
 
 <style lang="scss" scoped>
+.welcome-auth {
+  background-color: $color-brand-2-800;
+
+  > .header {
+    display: flex;
+    height: 100px;
+  }
+  > .header > .title {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 13px;
+    line-height: 18px;
+    color: #fff;
+  }
+  > .header > .logo {
+    width: 85px;
+    height: 30px;
+  }
+
+  > .footer {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  > .footer > .login {
+    background-color: #fff;
+    color: #000;
+    width: 155.5px;
+    height: 48px;
+    border-radius: 13px;
+    border: none;
+    text-align: center;
+    font-size: 17px;
+    font-family: Inter;
+    font-style: normal;
+  }
+  > .footer > .signup {
+    background-color: $color-ui-primary-500;
+    color: #fff;
+    width: 155.5px;
+    height: 48px;
+    border-radius: 13px;
+    border: none;
+    text-align: center;
+    font-size: 17px;
+    font-family: Inter;
+    font-style: normal;
+  }
+}
 </style>
