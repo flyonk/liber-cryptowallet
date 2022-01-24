171f1209b0299774dce6f894d35ce6684c2a8f15
Shutemov <shutemov1997@mail.ru>
2022-01-24 13:33:03 +0300

    Creates top navigation lines for StoresSlider
    Adds css styles according to the vue-agile documentation

--- src/components/UI/StoresSlider.vue
+++ src/components/UI/StoresSlider.vue
@@ -31,4 +31,43 @@ defineProps<Props>();
 </script>
 
 <style lang="scss">
+.agile {
+  display: flex;
+  flex-direction: column-reverse;
+
+  &__dots {
+    width: 100%;
+    background-color: $color-brand-2-400;
+  }
+
+  &__dot {
+    background-color: green;
+    width: 33%;
+    height: auto;
+    margin-left: 5px;
+  }
+
+  &__dot:first-child {
+    margin-left: 0;
+  }
+
+  &__dot > button {
+    background-color: transparent;
+    border: 1px solid #fff;
+    cursor: pointer;
+    display: block;
+    height: 5px;
+    font-size: 0;
+    line-height: 0;
+    margin: 0;
+    padding: 0;
+    transition-duration: 0.3s;
+    width: 100%;
+  }
+
+  &__dot:hover > button,
+  &__dot--current > button {
+    background-color: #fff;
+  }
+}
 </style>
