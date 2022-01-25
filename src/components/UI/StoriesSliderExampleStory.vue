72616aea40b6171906735b3084b1d4f3f68c9c32
Shutemov <shutemov1997@mail.ru>
2022-01-25 14:47:15 +0300

    Adds styles in StoriesSliderExampleStory

--- src/components/UI/StoriesSliderExampleStory.vue
+++ src/components/UI/StoriesSliderExampleStory.vue
@@ -32,6 +32,54 @@
 </script>
 
 <style lang="scss" scoped>
+.example-story {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: nowrap;
+    flex: 0 0 auto;
+    > .title {
+      font-family: Inter;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 13px;
+      line-height: 18px;
+      color: white;
+    }
+  }
+
+  > .main {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 auto;
+    > .title {
+      font-family: Inter;
+      font-style: normal;
+      font-weight: 800;
+      font-size: 34px;
+      line-height: 41px;
+      letter-spacing: 0.004em;
+      color: white;
+    }
+
+    > .imagecontainer {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+    > .image {
+      align-self: center;
+    }
+  }
+
+  > .footer {
+    display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: space-around;
