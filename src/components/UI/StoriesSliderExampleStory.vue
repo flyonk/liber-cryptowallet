fce81ab2da907a35d7c643aa099afc57fdaa91d9
Shutemov <shutemov1997@mail.ru>
2022-01-26 16:35:23 +0300

    Refactoring nesting of StoriesSliderExampleStory styles

--- src/components/UI/StoriesSliderExampleStory.vue
+++ src/components/UI/StoriesSliderExampleStory.vue
@@ -37,46 +37,51 @@
   flex-direction: column;
   height: 100%;
   background-color: $color-brand-2-800;
-  .header {
+  > .header {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
     flex-wrap: nowrap;
     flex: 0 0 auto;
-    > .title {
-      font-family: Inter;
-      font-style: normal;
-      font-weight: 600;
-      font-size: 13px;
-      line-height: 18px;
-      color: white;
-    }
+  }
+
+  > .header > .title {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 13px;
+    line-height: 18px;
+    color: white;
+    margin-left: 25px;
   }
 
   > .main {
     display: flex;
     flex-direction: column;
     flex: 1 0 auto;
-    > .title {
-      font-family: Inter;
-      font-style: normal;
-      font-weight: 800;
-      font-size: 34px;
-      line-height: 41px;
-      letter-spacing: 0.004em;
-      color: white;
-    }
+  }
 
-    > .imagecontainer {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-    }
-    > .image {
-      align-self: center;
-    }
+  > .main > .title {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 34px;
+    line-height: 41px;
+    letter-spacing: 0.004em;
+    color: white;
+    margin-left: 25px;
+  }
+
+  > .main > .imagecontainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  > .main > .imagecontainer > .image {
+    align-self: center;
   }
 
   > .footer {
@@ -86,31 +91,31 @@
     justify-content: space-around;
     flex: 0 0 auto;
     height: 85px;
+  }
 
-    > .login {
-      background-color: #fff;
-      color: #000;
-      width: 155.5px;
-      height: 48px;
-      border-radius: 13px;
-      border: none;
-      text-align: center;
-      font-size: 17px;
-      font-family: Inter;
-      font-style: normal;
-    }
-    > .signup {
-      background-color: $color-ui-primary-500;
-      color: #fff;
-      width: 155.5px;
-      height: 48px;
-      border-radius: 13px;
-      border: none;
-      text-align: center;
-      font-size: 17px;
-      font-family: Inter;
-      font-style: normal;
-    }
+  > .footer > .login {
+    background-color: #fff;
+    color: #000;
+    width: 155.5px;
+    height: 48px;
+    border-radius: 13px;
+    border: none;
+    text-align: center;
+    font-size: 17px;
+    font-family: Inter;
+    font-style: normal;
+  }
+  > .footer > .signup {
+    background-color: $color-ui-primary-500;
+    color: #fff;
+    width: 155.5px;
+    height: 48px;
+    border-radius: 13px;
+    border: none;
+    text-align: center;
+    font-size: 17px;
+    font-family: Inter;
+    font-style: normal;
   }
 }
 </style>
