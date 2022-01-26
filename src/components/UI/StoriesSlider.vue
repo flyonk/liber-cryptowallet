72638111162a485e06aeaef168a59dde9ed96ca2
Shutemov <shutemov1997@mail.ru>
2022-01-26 11:14:16 +0300

    Fixes StoriesSlider styles

--- src/components/UI/StoriesSlider.vue
+++ src/components/UI/StoriesSlider.vue
@@ -18,57 +18,60 @@ defineProps<props>();
 </script>
 
 <style lang="scss" scoped>
-.stories-slider ::v-deep .agile {
-  display: flex;
-  flex-direction: column-reverse;
+.stories-slider {
   height: 100%;
-
-  &__list,
-  &__slides,
-  &__slide,
-  &__track {
+  ::v-deep .agile {
+    display: flex;
+    flex-direction: column-reverse;
     height: 100%;
-  }
 
-  &__slide {
-    overflow: scroll;
-  }
+    &__list,
+    &__slides,
+    &__slide,
+    &__track {
+      height: 100%;
+    }
 
-  &__dots {
-    width: 100%;
-  }
+    &__slide {
+      overflow: scroll;
+    }
 
-  &__dot {
-    background-color: $color-brand-2-300;
-    width: 100%;
-    height: auto;
-    margin-left: 5px;
-    border-radius: 2px;
-  }
+    &__dots {
+      width: 100%;
+    }
 
-  &__dot:first-child {
-    margin-left: 0;
-  }
+    &__dot {
+      background-color: $color-brand-2-300;
+      width: 100%;
+      height: auto;
+      margin-left: 5px;
+      border-radius: 2px;
+    }
 
-  &__dot > button {
-    display: block;
-    width: 100%;
-    height: 2px;
-    margin: 0;
-    cursor: pointer;
-    background-color: $color-brand-2-300;
-    border: none;
-    font-size: 0;
-    line-height: 0;
-    padding: 0;
-    transition-duration: 0.3s;
-    border-radius: 2px;
-  }
+    &__dot:first-child {
+      margin-left: 0;
+    }
 
-  &__dot:hover > button,
-  &__dot--current > button {
-    background-color: #fff;
-    border-radius: 2px;
+    &__dot > button {
+      display: block;
+      width: 100%;
+      height: 2px;
+      margin: 0;
+      cursor: pointer;
+      background-color: $color-brand-2-300;
+      border: none;
+      font-size: 0;
+      line-height: 0;
+      padding: 0;
+      transition-duration: 0.3s;
+      border-radius: 2px;
+    }
+
+    &__dot:hover > button,
+    &__dot--current > button {
+      background-color: #fff;
+      border-radius: 2px;
+    }
   }
 }
 </style>
