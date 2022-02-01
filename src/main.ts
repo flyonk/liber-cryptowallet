2b40885b21ab75f16a4aee4a6756a66d166e23ea
Shutemov <shutemov1997@mail.ru>
2022-02-01 12:48:39 +0300

    Presumably structure of instances in Main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import { createPinia } from 'pinia';
 
+import AuthService from './services/AuthService';
+
 import '@/assets/styles/index.scss';
+import ApiService from './services/ApiService';
+
+const apiService = ApiService.getInstance()
+const authService = new AuthService(apiService)
+
 
 (async () => {
   const app = createApp(App).use(createPinia());
