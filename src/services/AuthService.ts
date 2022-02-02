4894b06770658d92db84f0efafed299bbd247987
Shutemov <shutemov1997@mail.ru>
2022-02-02 11:53:23 +0300

    Puts component interface to types folder

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,15 +1,7 @@
-// import { AxiosInstance } from "axios";
 import ApiService from "./ApiService";
+import { IAuthService } from "@/types/IAuthService";
 
 const URL = process.env.VUE_APP_AUTH_URL
-
-interface IAuthService {
-    url: string;
-    signIn(data: { phone: string }): void;
-    signInProceed(data: { phone: string, otp: string }): void
-    logout(data: { access_token: string }): void;
-    refresh(data: { access_token: string }): void;
-}
 
 class AuthService implements IAuthService {
     private static _instance: AuthService;
