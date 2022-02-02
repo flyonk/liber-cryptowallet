b6ddf222eb4ec5b26945b15761315860b22b17de
Shutemov <shutemov1997@mail.ru>
2022-02-02 18:38:19 +0300

    Updates IKycService

--- src/types/IKycService.ts
+++ src/types/IKycService.ts
@@ -1,12 +1,12 @@
+import { TSuccessResponse } from '@/types/TSuccessResponse';
 import { TErrorResponse } from './TErrorResponse';
-import { TClaim, TClaimFileList } from "./TClaim";
+import { TClaimFileList } from "./TClaim";
 
 export interface IKycService {
     kycCreateClaim(payload: any): Promise<TClaimFileList | TErrorResponse>
     kycHook(): void
     kycGetClaimById(id: number): Promise<TClaimFileList | TErrorResponse>
-    kycProceedClaimById(): void
-    kycDeleteClaim(): void
-    kycAddFileById(): void
-    kycDeleteFileById(): void
+    kycProceedClaimById(id: number): Promise<TSuccessResponse | TErrorResponse>
+    kycDeleteFileById(id:number, fileId:number): Promise<TSuccessResponse | TErrorResponse>
+    kycAddFileById(id:number, fileId:number): Promise<TSuccessResponse | TErrorResponse>
 }
