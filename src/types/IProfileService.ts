9490557ead55f2c8c29f72e349d50a6f47f4136e
Shutemov <shutemov1997@mail.ru>
2022-02-02 17:49:24 +0300

    Updates IProfileService

--- src/types/IProfileService.ts
+++ src/types/IProfileService.ts
@@ -1,7 +1,9 @@
-import { AxiosPromise } from 'axios';
+import { TSuccessResponse } from './TSuccessResponse';
+import { TErrorResponse } from './TErrorResponse';
 import { TProfile } from './TProfile';
+
 export interface IProfileService {
-    getProfile(): Promise<AxiosPromise<TProfile> | undefined>;
-    updateProfile(): void;
-    updateProfileAvatar(): void;
+    getProfile(): Promise<TProfile | TErrorResponse>;
+    updateProfile(payload: TProfile): Promise<TProfile | TErrorResponse>;
+    updateProfileAvatar(file: File): Promise<TSuccessResponse | TErrorResponse>;
 }
