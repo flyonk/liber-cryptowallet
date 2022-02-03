39cd9d8bfa9373efc437ee2220fb62a9dcd77ae3
Shutemov <shutemov1997@mail.ru>
2022-02-03 11:26:07 +0300

    Updates IAuthService returning types

--- src/types/IAuthService.ts
+++ src/types/IAuthService.ts
@@ -1,6 +1,9 @@
+import { TSuccessSignIn } from './TSuccessSignIn';
+import { TSuccessResponse } from './TSuccessResponse';
+import { TErrorResponse } from './TErrorResponse';
 export interface IAuthService {
-    signIn(data: { phone: string }): void;
-    signInProceed(data: { phone: string, otp: string }): void
-    logout(data: { access_token: string }): void;
-    refresh(data: { access_token: string }): void;
+    signIn(data: { phone: string }): Promise<TSuccessResponse | TErrorResponse>;
+    signInProceed(data: { phone: string, otp: string }): Promise<TSuccessSignIn | TErrorResponse>
+    logout(data: { access_token: string }): Promise<TSuccessResponse | TErrorResponse>;
+    refresh(data: { access_token: string }): Promise<TSuccessSignIn | TErrorResponse>;
 }
