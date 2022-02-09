183f37b4a13ded343124b40f17a909d69469d924
Shutemov <shutemov1997@mail.ru>
2022-02-09 14:23:17 +0300

    Implements PrivacySettings script

--- src/views/Dashboard/Account/PrivacySettings.vue
+++ src/views/Dashboard/Account/PrivacySettings.vue
@@ -2,6 +2,12 @@
 </template>
 
 <script setup lang="ts">
+import { BaseSwitch } from '@/components/UI';
+import { ref } from 'vue';
+
+let isEmailsAgreement = ref(true);
+let isPushesAgreement = ref(true);
+let isSocialMediaAgreement = ref(true);
 </script>
 
 <style lang="scss">
