16bd1f13795f448005baaffbc05d8f484230e99d
Shutemov <shutemov1997@mail.ru>
2022-02-16 17:05:45 +0300

    Adds RequestFundsService interface

--- src/types/api.ts
+++ src/types/api.ts
@@ -21,6 +21,44 @@ export interface IKycService {
     kycAddFileById(id: number, fileId: number): Promise<TSuccessResponse | TErrorResponse>
 }
 
+/**
+ *   RequestFundsService
+ */
+
+export interface IRequestFundsService {
+    downloadStatement(id: string): Promise<TStatement | TErrorResponse>
+    getPaymentLink(id: string): Promise<TPaymentLink | TErrorResponse>
+    cancelRequestFunds(id: string): Promise<TSuccessResponse | TErrorResponse>
+    sendRequestedFunds(id: string): Promise<TSuccessResponse | TErrorResponse>
+    declineRequestedFunds(id: string): Promise<TSuccessResponse | TErrorResponse>
+    createRequestFunds(id: string, data: TCreateRequestFundsData): Promise<TSuccessResponse | TErrorResponse>
+}
+
+export type TCreateRequestFundsData = {
+    amount: string,
+    account: string,
+    network?: string,
+    targetUser: {
+        id?: string,
+        email?: string,
+        phone?: string
+    }
+}
+
+export type TStatement = {
+    statement: File
+}
+
+export type TPaymentLink = {
+    id: string
+    paymentLink: string,
+    expire: string
+}
+
+/**
+ *   ProfileService
+ */
+
 export interface IProfileService {
     getProfile(): Promise<TProfile | TErrorResponse>;
     updateProfile(payload: TUpdateProfile): Promise<TProfile | TErrorResponse>;
