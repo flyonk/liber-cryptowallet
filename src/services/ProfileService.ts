fac7eeeb56748e44eb39e5b6e8f49fa4241cb014
Shutemov <shutemov1997@mail.ru>
2022-02-16 18:54:03 +0300

    Removes access_token from url in ProfileService

--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -6,7 +6,6 @@ import { TSuccessResponse } from '@/types/api';
 import { IApiService } from '@/types/api';
 import { TErrorResponse } from '@/types/api';
 import { TProfile } from '@/types/api';
-import { useAccountStore } from '@/stores/account';
 import { IKycService } from '@/types/api';
 import { IProfileService } from '@/types/api';
 import ApiService from './ApiService';
@@ -30,68 +29,60 @@ class ProfileService implements IProfileService, IKycService {
     }
 
     async getProfile() {
-        const store = useAccountStore()
-        const url = `${this.url}?access_token=${store.token}`
+        const url = `${this.url}`
         const res: TProfile | TErrorResponse = await this._apiServiceInstance.fetch.get(url)
         return res
     }
 
-    async updateProfile(payload: TUpdateProfile) {
-        const store = useAccountStore()
-        const url = `${this.url}?access_token=${store.token}`
-        const res: TProfile | TErrorResponse = await this._apiServiceInstance.fetch.patch(url, payload)
+    async updateProfile(data: TUpdateProfile) {
+        const url = `${this.url}`
+        const res: TProfile | TErrorResponse = await this._apiServiceInstance.fetch.patch(url, data)
         return res
     }
 
-    async updateProfileAvatar(payload: File) {
-        const store = useAccountStore()
-        const url = `${this.url}?access_token=${store.token}`
+    async updateProfileAvatar(data: File) {
+        const url = `${this.url}`
         const config = {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         }
-        const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.patch(url, payload, config)
+        const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.patch(url, data, config)
         return res
     }
 
-    async kycCreateClaim(payload: any) {
-        const store = useAccountStore()
-        const url = `${this.url}/kyc/claim?access_token=${store.token}`
-        const res: TClaimFileList | TErrorResponse = await this._apiServiceInstance.fetch.post(url, payload)
+    async kycCreateClaim(data: any) {
+        const url = `${this.url}/kyc/claim`
+        const res: TClaimFileList | TErrorResponse = await this._apiServiceInstance.fetch.post(url, data)
         return res
     }
 
-    async kycHook(payload: any) {
+    async kycHook(data: any) {
         const url = `${this.url}/kyc/claim/hook`
-        const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.post(url, payload)
+        const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.post(url, data)
         return res
     }
 
     async kycGetClaimById(id: number) {
-        const store = useAccountStore()
-        const url = `${this.url}/kyc/claim/${id}?access_token=${store.token}`
+        const url = `${this.url}/kyc/claim/${id}`
         const res: TClaimFileList | TErrorResponse = await this._apiServiceInstance.fetch.get(url)
         return res
     }
 
     async kycProceedClaimById(id: number) {
-        const store = useAccountStore()
-        const url = `${this.url}/kyc/claim/${id}/proceed?access_token=${store.token}`
+        const url = `${this.url}/kyc/claim/${id}/proceed`
         const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.post(url)
         return res
     }
 
     async kycDeleteFileById(id: number, fileId: number) {
-        const store = useAccountStore()
-        const url = `${this.url}/kyc/claim/${id}/file/${fileId}?access_token=${store.token}`
+        const url = `${this.url}/kyc/claim/${id}/file/${fileId}`
         const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.delete(url)
         return res
     }
 
     async kycAddFileById(id: number, fileId: number) {
-        const store = useAccountStore()
-        const url = `${this.url}/kyc/claim/${id}/file/${fileId}?access_token=${store.token}`
+        const url = `${this.url}/kyc/claim/${id}/file/${fileId}`
         const res: TSuccessResponse | TErrorResponse = await this._apiServiceInstance.fetch.post(url)
         return res
     }
