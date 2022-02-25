724a88ddc1f500f2893f7c0903e34764ea28dba8
shutemov <shutemov1997@mail.ru>
2022-02-25 18:34:53 +0700

    Minor fixes in country-phone

--- src/services/country-phone.ts
+++ src/services/country-phone.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { ICountryInformation } from '@/types/country-phone-types';
 
-export const getFullList = async (): Promise<Array<ICountryInformation>> => {
+export const getFullList = async (): Promise<ICountryInformation[]> => {
   const { data } = await axios.get('/country_dial_info.json');
-
   return data;
 };
