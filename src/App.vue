59c27191392e14844331bbf9f3d933c9f003d7f0
marselm@cryptowize.tech <marselm@cryptowize.tech>
2022-02-25 11:37:19 +0600

    add safe area

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     </template>
   </p-toast>
 
-  <app-layout-switcher>
+  <app-layout-switcher :style="{ 'padding-top': `${padding.top}px`, 'padding-bottom': `${padding.bottom}px`, 'padding-left': `${padding.left}px` , 'padding-right': `${padding.right}px` }">
     <router-view class="router-view" />
   </app-layout-switcher>
 </template>
@@ -21,12 +21,26 @@
 import PToast from 'primevue/toast';
 import AppLayoutSwitcher from './components/Common/AppLayoutSwitcher.vue';
 import { useAccountStore } from './stores/account';
+import { SafeArea } from "capacitor-plugin-safe-area";
+import { ref } from 'vue';
 
 const store = useAccountStore();
 store.init();
+
+const padding = ref({
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0
+});
+
+SafeArea.getSafeAreaInsets().then(({ insets }) => {
+  Object.assign(padding.value, insets)
+  console.debug(padding.value)
+})
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .main-nav {
   padding: 30px;
 
