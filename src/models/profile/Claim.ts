6a115e395abf270ddb40b658e660f75d686ca3bd
shutemov <shutemov1997@mail.ru>
2022-03-03 18:22:40 +0700

    Updates Claim model

--- src/models/profile/Claim.ts
+++ src/models/profile/Claim.ts
@@ -1,5 +1,39 @@
 export interface IClaim {
     id: number,
     userId: string,
-    status:number
+    status: number,
+    fileList: IClaimFile[]
 }
+
+interface IClaimFile {
+    id: number,
+    claimId: number,
+    type: number,
+    name: string,
+    extension: string,
+    link: string,
+    size: number,
+    createdAt: string,
+    updatedAt: string,
+    deletedAt: string
+}
+
+export default {
+    /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    deserialize(input: any): IClaim {
+        const { id, userId, status, fileList } = input
+
+        return {
+            id, userId, status, fileList
+        };
+    },
+
+    /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    requestSerialize(input: IClaim): any {
+        const { id, userId, status, fileList } = input
+
+        return {
+            id, userId, status, fileList
+        };
+    },
+};
