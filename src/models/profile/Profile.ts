b152ea7f24f9ccb613c520bc33b10a5c4ad74740
shutemov <shutemov1997@mail.ru>
2022-03-03 18:22:18 +0700

    Updates Profile model

--- src/models/profile/Profile.ts
+++ src/models/profile/Profile.ts
@@ -10,47 +10,84 @@ export interface IProfile {
     streetAndNumber: string;
     optionalAddress: string;
     postalCode: string;
-    birthDate: string
-    isSendNews: boolean
+    birthDate: string,
+    isSendNews: boolean,
+    avatarLink: string
 }
 
 
 export default {
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     deserialize(input: any): IProfile {
+        const {
+            status,
+            phone,
+            isVerified,
+            blockReason,
+            firstName,
+            lastName,
+            email,
+            country,
+            streetAndNumber,
+            optionalAddress,
+            postalCode,
+            birthDate,
+            isSendNews,
+            avatarLink
+        } = input
+
         return {
-            status: input.status,
-            phone: input.phone,
-            isVerified: input.isVerified,
-            blockReason: input.blockReason,
-            firstName: input.firstName,
-            lastName: input.lastName,
-            email: input.email,
-            country: input.country,
-            streetAndNumber: input.streetAndNumber,
-            optionalAddress: input.optionalAddress,
-            postalCode: input.postalCode,
-            birthDate: input.birthDate,
-            isSendNews: input.isSendNews
+            status,
+            phone,
+            isVerified,
+            blockReason,
+            firstName,
+            lastName,
+            email,
+            country,
+            streetAndNumber,
+            optionalAddress,
+            postalCode,
+            birthDate,
+            isSendNews,
+            avatarLink
         };
     },
 
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-    requestSerialize(input: IProfile): any {
+    requestSerialize(input: Partial<IProfile>): any {
+        const {
+            status,
+            phone,
+            isVerified,
+            blockReason,
+            firstName,
+            lastName,
+            email,
+            country,
+            streetAndNumber,
+            optionalAddress,
+            postalCode,
+            birthDate,
+            isSendNews,
+            avatarLink
+        } = input
+
         return {
-            status: input.status,
-            phone: input.phone,
-            is_verified: input.isVerified,
-            block_reason: input.blockReason,
-            first_name: input.firstName,
-            last_name: input.lastName,
-            email: input.email,
-            country: input.country,
-            street_and_number: input.streetAndNumber,
-            optional_address: input.optionalAddress,
-            postal_code: input.postalCode,
-            birth_date: input.birthDate,
-            is_send_news: input.isSendNews
+            status,
+            phone,
+            isVerified,
+            blockReason,
+            firstName,
+            lastName,
+            email,
+            country,
+            streetAndNumber,
+            optionalAddress,
+            postalCode,
+            birthDate,
+            isSendNews,
+            avatarLink
         };
     },
 };
