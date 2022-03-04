e6e5ed68a39960ec2509cc3f734d0722f3ab9620
shutemov <shutemov1997@mail.ru>
2022-03-04 12:23:49 +0700

    Creates report domain

---
+++ src/models/report/Report.ts
@@ -0,0 +1,21 @@
+export interface IReport {
+    type: string
+    text: string
+}
+
+export default {
+    /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    deserialize(input: any): IReport {
+        return {
+            type: input.type,
+            text: input.text
+        };
+    },
+    /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    requestSerialize(input: IReport): any {
+        return {
+            type: input.type,
+            text: input.text
+        };
+    },
+};
