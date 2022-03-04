fbdc5293e0930e2922e3453e9bcd7beafcc7a9b4
shutemov <shutemov1997@mail.ru>
2022-03-04 11:51:12 +0700

    Updates recipient domain

--- src/models/recipient/IPhoneContact.ts
+++ src/models/recipient/IPhoneContact.ts
@@ -7,18 +7,19 @@ export interface IIPhoneContact {
 export default {
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     deserialize(input: any): IIPhoneContact {
-        const { name, emails, phones } = input
-
         return {
-            name, emails, phones
+            name: input.name, 
+            emails: input.emails, 
+            phones: input.phones
         };
     },
+
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     requestSerialize(input: IIPhoneContact): any {
-        const { name, emails, phones } = input
-
         return {
-            name, emails, phones
+            name: input.name,
+            emails: input.emails,
+            phones: input.phones,
         };
     },
 };
--- src/models/recipient/LiberContact.ts
+++ src/models/recipient/LiberContact.ts
@@ -13,18 +13,34 @@ type TCommunicationInfo = {
 export default {
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     deserialize(input: any): ILiberContact {
-        const { id, name, phones, emails } = input
-
         return {
-            id, name, phones, emails
+            id: input.id,
+            name: input.name,
+            phones: deserializeCommunicationInfoAdaptor(input.phones),
+            emails: deserializeCommunicationInfoAdaptor(input.emails),
         };
     },
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     requestSerialize(input: ILiberContact): any {
-        const { id, name, phones, emails } = input
-
         return {
-            id, name, phones, emails
+            id: input.id,
+            name: input.name,
+            phones: requestSerializeCommunicationInfoAdaptor(input.phones),
+            emails: requestSerializeCommunicationInfoAdaptor(input.emails)
         };
     },
 };
+
+const deserializeCommunicationInfoAdaptor = (input: any): TCommunicationInfo[] => {
+    return input.map((e: any) => {
+        const { value, is_primary } = e
+        return { value, isPrimary: is_primary }
+    })
+}
+
+const requestSerializeCommunicationInfoAdaptor = (input: TCommunicationInfo[]): any => {
+    return input.map((e: TCommunicationInfo) => {
+        const { value, isPrimary } = e
+        return { value, is_primary: isPrimary }
+    })
+} 
