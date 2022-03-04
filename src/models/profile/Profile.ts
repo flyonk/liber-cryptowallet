de81b01cfe632bb4b3b97a9822b7838a54010bf4
shutemov <shutemov1997@mail.ru>
2022-03-04 11:05:00 +0700

    Fixes Profile model

--- src/models/profile/Profile.ts
+++ src/models/profile/Profile.ts
@@ -1,93 +1,59 @@
 export interface IProfile {
-    status: number;
-    phone: string;
-    isVerified: false;
-    blockReason: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    country: string;
-    streetAndNumber: string;
-    optionalAddress: string;
-    postalCode: string;
-    birthDate: string,
-    isSendNews: boolean,
-    avatarLink: string
+    status?: number;
+    phone?: string;
+    isVerified?: false;
+    blockReason?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    country?: string;
+    street?: string;
+    homeNum?: string;
+    optionalAddress?: string;
+    postalCode?: string;
+    birthDate?: string,
+    isSendNews?: boolean,
 }
 
 
 export default {
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
     deserialize(input: any): IProfile {
-        const {
-            status,
-            phone,
-            isVerified,
-            blockReason,
-            firstName,
-            lastName,
-            email,
-            country,
-            streetAndNumber,
-            optionalAddress,
-            postalCode,
-            birthDate,
-            isSendNews,
-            avatarLink
-        } = input
 
         return {
-            status,
-            phone,
-            isVerified,
-            blockReason,
-            firstName,
-            lastName,
-            email,
-            country,
-            streetAndNumber,
-            optionalAddress,
-            postalCode,
-            birthDate,
-            isSendNews,
-            avatarLink
+            status: input.status,
+            phone: input.phone,
+            isVerified: input.is_verified,
+            blockReason: input.block_reason,
+            firstName: input.first_name,
+            lastName: input.last_name,
+            email: input.email,
+            country: input.country,
+            street: input?.street_and_number.split(' ')[0],
+            homeNum: input?.street_and_number.split(' ')[1],
+            optionalAddress: input.otional_address,
+            postalCode: input.postal_code,
+            birthDate: input.birthdate,
+            isSendNews: input.is_send_news,
         };
     },
 
     /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-    requestSerialize(input: Partial<IProfile>): any {
-        const {
-            status,
-            phone,
-            isVerified,
-            blockReason,
-            firstName,
-            lastName,
-            email,
-            country,
-            streetAndNumber,
-            optionalAddress,
-            postalCode,
-            birthDate,
-            isSendNews,
-            avatarLink
-        } = input
-
+    requestSerialize(input: IProfile): any {
         return {
-            status,
-            phone,
-            isVerified,
-            blockReason,
-            firstName,
-            lastName,
-            email,
-            country,
-            streetAndNumber,
-            optionalAddress,
-            postalCode,
-            birthDate,
-            isSendNews,
-            avatarLink
+            status: input.status,
+            phone: input.phone,
+            is_verified: input.isVerified,
+            block_reason: input.blockReason,
+            first_name: input.firstName,
+            last_name: input.lastName,
+            email: input.email,
+            country: input.country,
+            street_and_number: `${input.street} ${input.homeNum}`,
+            optionalAddress: input.optionalAddress,
+            postal_code: input.postalCode,
+            birthdate: input.birthDate,
+            is_send_news: input.isSendNews,
         };
     },
 };
