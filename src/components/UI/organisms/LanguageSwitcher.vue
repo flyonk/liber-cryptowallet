366eaa56935444fb064461437ee57f26fc84f197
eubash <nemezidaxxx@mail.ru>
2022-03-10 10:58:41 +0100

    feature: add axios interceptor plugin

--- src/components/UI/organisms/LanguageSwitcher.vue
+++ src/components/UI/organisms/LanguageSwitcher.vue
@@ -1,23 +1,16 @@
 <template>
   <!-- TODO: this is only stub component, need to be styled when layout is ready -->
-  <select
-    v-model="locale"
-    @change="changeLang"
-  >
-    <option
-      v-for="(item, i) in availableLocales"
-      :key="i"
-      :value="item"
-    >
+  <select v-model="locale" @change="changeLang">
+    <option v-for="(item, i) in availableLocales" :key="i" :value="item">
       {{ item }}
     </option>
   </select>
 </template>
 
 <script setup lang="ts">
-import { useI18n } from "vue-i18n";
-import {Storage} from "@capacitor/storage";
-import {EStorageKeys} from "@/types/base-component";
+import { useI18n } from 'vue-i18n';
+import { Storage } from '@capacitor/storage';
+import { EStorageKeys } from '@/types/base-component';
 
 const { locale, availableLocales } = useI18n({ useScope: 'global' });
 
@@ -25,11 +18,8 @@ function changeLang(): void {
   Storage.set({
     key: EStorageKeys.language,
     value: locale.value,
-  })
+  });
 }
-
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
