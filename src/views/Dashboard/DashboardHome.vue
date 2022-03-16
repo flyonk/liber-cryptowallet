d04a8a50190b3bb9251770d816c0f98cd0bc6e49
shutemov <shutemov1997@mail.ru>
2022-03-16 12:00:19 +0700

    Adds transaction adaptor by type in DashboardHome

--- src/views/Dashboard/DashboardHome.vue
+++ src/views/Dashboard/DashboardHome.vue
@@ -109,37 +109,28 @@
               @click="$router.push('/transactions/details')"
             >
               <div class="image">
-                <img
-                  v-if="transaction.type === 'Deposit'"
-                  class="icon"
-                  src="@/assets/icon/transactions/received.svg"
-                />
-                <img
-                  v-else-if="transaction.type === 'Send'"
-                  class="icon"
-                  src="@/assets/icon/transactions/sent.svg"
-                />
-                <!-- <img class="icon" :src="transaction.img" /> -->
+                <img class="icon" :src="transaction.img" />
               </div>
               <div class="info">
                 <div class="flex">
-                  <h1 class="title">test</h1>
-                  <p class="title">{{ transaction.amount }}</p>
-                  <!-- <h1 class="title">{{ transaction.info }}</h1> -->
-                  <!-- <p :class="{ received: transaction.sum.startsWith('+') }">
+                  <h1 class="title">{{ transaction.info }}</h1>
+                  <p
+                    class="sum"
+                    :class="{ '-received': transaction.sum.startsWith('+') }"
+                  >
                     {{ transaction.sum }}
-                  </p> -->
+                  </p>
                 </div>
                 <div class="flex">
-                  <!-- <div class="subtitle">{{ transaction.from }}</div> -->
-                  <div class="subtitle">transaction from</div>
+                  <div class="subtitle">
+                    {{ transaction.contractor }}
+                  </div>
                   <p
-                    v-if="transaction.status"
+                    v-if="transaction.status === 'Pending'"
                     :class="{ pending: transaction.status === 'Pending' }"
                   >
                     {{ transaction.status }}
                   </p>
-                  <p v-else class="sum">{{ transaction.amount }}</p>
                 </div>
               </div>
             </li>
@@ -194,6 +185,7 @@ import transactionService from '@/services/transactionService';
 import {
   ERequestFundsStatus,
   ETransactionStatus,
+  ETransactionType,
   TTransaction,
 } from '@/models/transaction/transaction';
 
@@ -201,26 +193,62 @@ let transactions: any = ref([]);
 
 onMounted(async () => {
   transactions.value = await transactionService.getUserTransaction();
-  transactions.value = transactions.value.map(
-    (
-      e: TTransaction
-    ): {
-      info: string;
-      from: string;
-      sum: string;
-      status: ETransactionStatus | ERequestFundsStatus;
-      img: string;
-    } => {
-      return {
-        info: `${tm('transactions.operations.received')} BTC`,
-        from: 'email@example.com',
-        sum: e.amount,
-        status: e.status,
-        img: '',
-      };
-    }
-  );
+
+  transactions.value = transactions.value.map(_getTransactionDataByType);
 });
+
+const _getTransactionDataByType = (
+  transaction: TTransaction
+): {
+  info: string;
+  contractor: string;
+  sum: string;
+  status: ETransactionStatus | ERequestFundsStatus;
+  img: string;
+} => {
+  if (transaction.type === ETransactionType.Deposit)
+    return {
+      info: `${tm('transactions.operations.deposit')} ${transaction.code}`,
+      contractor: `From Bitcoin address`, //TODO: resolve it
+      sum: `+ ${transaction.amount} ${transaction.code}`,
+      status: transaction.status,
+      img: require('@/assets/icon/transactions/deposit.svg'),
+    };
+
+  if (transaction.type === ETransactionType.Received)
+    return {
+      info: `${tm('transactions.operations.received')} ${transaction.code}`,
+      contractor: `From ${
+        transaction.contractor.email
+          ? transaction.contractor.email
+          : transaction.contractor.phone
+      }`, //TODO: fix it only by email
+      sum: `+ ${transaction.amount} ${transaction.code}`,
+      status: transaction.status,
+      img: require('@/assets/icon/transactions/received.svg'),
+    };
+
+  if (transaction.type === ETransactionType.Send)
+    return {
+      info: `${tm('transactions.operations.sent')} ${transaction.code}`,
+      contractor: `To ${
+        transaction.contractor.email
+          ? transaction.contractor.email
+          : transaction.contractor.phone
+      }`, //TODO: fix it only by email
+      sum: `- ${transaction.amount} ${transaction.code}`,
+      status: transaction.status,
+      img: require('@/assets/icon/transactions/sent.svg'),
+    };
+
+  return {
+    info: '',
+    contractor: '',
+    sum: '',
+    status: ETransactionStatus.Pending,
+    img: '',
+  };
+};
 
 let activeTab = ref(1);
 const VerificationStatus = ref('verified');
@@ -282,30 +310,6 @@ const carousel = [
     text: 'black',
   },
 ];
-
-// const transactions = ref([
-//   {
-//     info: `${tm('transactions.operations.received')} BTC`,
-//     from: `${tm('common.from')} test@cryptowize.tech`,
-//     sum: '+ 0.0001 BTC',
-//     status: 'Completed',
-//     img: require('@/assets/icon/transactions/received.svg'),
-//   },
-//   {
-//     info: `${tm('transactions.operations.received')} BTC`,
-//     from: `${tm('common.from')} test@cryptowize.tech`,
-//     sum: '+ 0.0001 BTC',
-//     status: 'Completed',
-//     img: require('@/assets/icon/transactions/received.svg'),
-//   },
-//   {
-//     info: `${tm('transactions.operations.received')} BTC`,
-//     from: `${tm('common.from')} test@cryptowize.tech`,
-//     sum: '+ 0.0001 BTC',
-//     status: 'Completed',
-//     img: require('@/assets/icon/transactions/received.svg'),
-//   },
-// ]);
 
 const hasTransactions = computed(() => transactions.value.length > 0);
 </script>
@@ -514,18 +518,22 @@ const hasTransactions = computed(() => transactions.value.length > 0);
 
               > .pending {
                 color: $color-yellow-600;
-              }
-
-              > .received {
-                color: $color-green-600;
-              }
-
-              > .sum {
+                font-style: normal;
                 font-size: 13px;
                 line-height: 18px;
                 text-align: right;
                 letter-spacing: -0.0008em;
-                color: $color-dark-grey;
+              }
+
+              > .sum {
+                font-weight: 500;
+                font-size: 16px;
+                line-height: 21px;
+                letter-spacing: -0.0031em;
+
+                &.-received {
+                  color: $color-green-600;
+                }
               }
             }
           }
