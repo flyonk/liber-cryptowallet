9507efbcefcd0c2dd024203a466f5b791135eadd
AleksStek <AleksStek@yandex.ru>
2022-03-17 09:01:20 +0700

    fix styles for rscss rules

--- src/components/ui/organisms/BaseCountryEntitySelect.vue
+++ src/components/ui/organisms/BaseCountryEntitySelect.vue
@@ -16,12 +16,12 @@
         <div
           v-for="(country, index) in filteredList"
           :key="index"
-          :class="{ selected: isSelectedCountry(country) }"
+          :class="{ '-selected': isSelectedCountry(country) }"
           class="item grid align-items-center"
           @click="setSelectedCountry(country)"
         >
           <div class="flag col-2">
-            <img :src="country.flag" alt="" />
+            <img :src="country.flag" alt="" class="img" />
           </div>
           <div class="code col-2">
             {{ country[entity] }}
@@ -89,47 +89,47 @@ function close() {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .country-select-block {
   padding-top: 16px;
+}
 
-  .cancel-button {
-    color: $color-primary;
+.cancel-button {
+  color: $color-primary;
+  cursor: pointer;
+  user-select: none;
+  user-select: none;
+  user-select: none;
+  user-select: none;
+}
+
+.country-list {
+  margin-top: 20px;
+
+  & > .item {
+    border-radius: 8px;
+    padding: 12px 16px;
+    margin-bottom: 8px;
     cursor: pointer;
-    user-select: none;
-    user-select: none;
-    user-select: none;
-    user-select: none;
-  }
 
-  .country-list {
-    margin-top: 20px;
-
-    .item {
-      border-radius: 8px;
-      padding: 12px 16px;
-      margin-bottom: 8px;
-      cursor: pointer;
-
-      &.selected {
-        background: $color-light-grey-300;
-      }
-
-      .flag {
-        padding: 0;
-
-        img {
-          object-fit: cover;
-          border-radius: 50%;
-          height: 40px;
-          width: 40px;
-        }
-      }
-
-      .code {
-        color: $color-dark-grey;
-      }
+    &.-selected {
+      background: $color-light-grey-300;
     }
   }
+}
+
+.flag {
+  padding: 0;
+
+  & > .img {
+    object-fit: cover;
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+  }
+}
+
+.code {
+  color: $color-dark-grey;
 }
 </style>
