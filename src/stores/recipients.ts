f5fa17ae7dd0d9b38ffb81136177a158bce25b95
AleksStek <AleksStek@yandex.ru>
2022-03-22 17:38:44 +0700

    Add mocked phone numbers for web testing

--- src/stores/recipients.ts
+++ src/stores/recipients.ts
@@ -3,6 +3,9 @@ import { Contacts } from '@capacitor-community/contacts';
 
 import { Contact } from '@/types/contacts';
 
+import { mockedContacts } from '@/helpers/mockData';
+import { CAPACITOR_WEB_ERROR } from '@/constants';
+
 interface IRecepients {
   contacts: Contact[];
   permission: boolean;
@@ -22,14 +25,21 @@ export const useRecepientsStore = defineStore('recepients', {
 
   actions: {
     async getPhoneContacts() {
-      const { granted } = await Contacts.getPermissions();
-      this.permission = !!granted;
-      if (granted) {
-        const { contacts } = await Contacts.getContacts();
-        this.contacts = contacts;
+      try {
+        const result = await Contacts.getPermissions();
+        const { granted } = result;
+        this.permission = !!granted;
+        if (granted) {
+          const { contacts } = await Contacts.getContacts();
+          this.contacts = contacts;
+          return true;
+        }
         return true;
+      } catch (error: any) {
+        if (error?.code === CAPACITOR_WEB_ERROR) {
+          this.contacts = mockedContacts;
+        }
       }
-      return true;
     },
   },
 });
