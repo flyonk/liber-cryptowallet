1f01daa481730096c8a0fdb2e7e56e5ff5630920
eubash <nemezidaxxx@mail.ru>
2022-03-31 17:26:56 +0200

    fix: user status login / signup

---
+++ src/services/userStatusService.ts
@@ -0,0 +1,26 @@
+import { useAuthStore } from '@/stores/auth';
+
+import { Route } from '@/router/types';
+import { EKYCStatus, EUserStatus, IProfile } from '@/models/profile/profile';
+
+export async function getKYCStatus(): Promise<void> {
+  //This is stub method to retrieve user status
+}
+
+export function getAuthRoute(user: IProfile): string {
+  const authStore = useAuthStore();
+  switch (user.status) {
+    case EUserStatus.authenticated:
+      authStore.setStep(2, 'registration');
+      return Route.SignUp;
+    case EUserStatus.registered:
+      if (user.kycStatus > EKYCStatus.not_started) {
+        return Route.Survey;
+      } else {
+        return Route.KYCMain;
+      }
+
+    default:
+      return Route.WelcomeLogoScreen;
+  }
+}
