ababb23a6afb4275015520048aa7c2614547c3ab
shutemov <shutemov1997@mail.ru>
2022-04-15 17:54:03 +0300

    Code example

---
+++ src/components/ui/molecules/SwiperStoriesSlider.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="">
+    <swiper
+      :modules="[Pagination, Autoplay]"
+      :loop="true"
+      :pagination="pagination"
+    >
+      <slot />
+    </swiper>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper } from 'swiper/vue';
+import 'swiper/css/pagination'; //TODO: move to index.css
+
+// import required modules
+import { Pagination } from 'swiper';
+
+const pagination = {
+  clickable: true,
+  type: 'custom',
+  renderCustom: function (swiper, current, total) {
+    //TODO: implement timeline logic
+    console.log('swiper', swiper.pagination.el);
+    const paginationItem = document.createElement('div');
+    paginationItem.classList.add('test');
+    paginationItem.innerHTML = `${current}/${total}`;
+    swiper.pagination.el.innerHTML = '';
+    swiper.pagination.el.append(paginationItem);
+
+    // return `<div class=''>${current}/${total}</div>`; //other way
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+:deep(.test) {
+  background-color: red;
+}
+
+:deep(.swiper) {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+:deep(.swiper-pagination) {
+  //   background-color: red;
+  position: relative;
+  display: flex;
+  bottom: 0;
+}
+
+:deep(.swiper-slide) {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+:deep(.swiper-pagination-bullet) {
+  height: 2px;
+  width: 100%;
+  color: #000;
+  opacity: 1;
+  background: rgb(0 0 0 / 20%);
+  border-radius: 0;
+  border: 0;
+}
+
+:deep(.swiper-pagination-bullet-active) {
+  color: #fff;
+  background: #007aff;
+}
+</style>
--- src/views/Welcome/WelcomeAuthScreen.vue
+++ src/views/Welcome/WelcomeAuthScreen.vue
@@ -1,16 +1,8 @@
 <template>
   <section class="welcome-auth" :style="stylePaddings">
-    <div class="slider">
-      <stories-slider
-        :options="options"
-        :is-hidden="false"
-        :on-overlay="false"
-        @after-change="handleAutoplayChange"
-        @change-by-user="handleChangeByUser"
-      >
-        <stories-slider-example-story
-          v-for="item in 2"
-          :key="item"
+    <swiper-stories-slider>
+      <SwiperSlide v-for="item in 15" :key="item"
+        ><stories-slider-example-story
           :base-img="require('@/assets/images/money.svg')"
           :logo-img="require('@/assets/images/full-logo-white.png')"
           :header-title="$t('auth.signup.welcomeAuth.title')"
@@ -19,45 +11,27 @@
           :sign-up-title="$t('auth.signup.welcomeAuth.signUp')"
           @login="$router.push({ name: Route.Login })"
           @signup="$router.push({ name: Route.SignUp })"
-        />
-      </stories-slider>
-    </div>
+      /></SwiperSlide>
+      <!-- <SwiperSlide><div>456</div></SwiperSlide> -->
+      ></swiper-stories-slider
+    >
   </section>
 </template>
 
 <script setup lang="ts">
-import StoriesSlider from '@/components/ui/molecules/StoriesSlider.vue';
 import StoriesSliderExampleStory from '@/components/ui/organisms/StoriesSliderExampleStory.vue';
+import SwiperStoriesSlider from '@/components/ui/molecules/SwiperStoriesSlider.vue';
+
 import useSafeAreaPaddings from '@/helpers/safeArea';
-import { ISlider } from '@/types/ISlider';
+import { SwiperSlide } from 'swiper/vue';
 import { Route } from '@/router/types';
 
 const { stylePaddings } = useSafeAreaPaddings();
-
-const options: ISlider = {
-  autoplay: true,
-  autoplaySpeed: 5000,
-  dots: true,
-  navButtons: false,
-  infinite: true,
-};
-
-const handleAutoplayChange = () => {
-  console.log('Check after change event by autoplay');
-};
-
-const handleChangeByUser = async () => {
-  console.log('Check after change event by user');
-};
 </script>
 
 <style lang="scss" scoped>
 .welcome-auth {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
   height: 100vh;
-  overflow: hidden;
 
   > .slider {
     overflow: scroll;
