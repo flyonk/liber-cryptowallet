9715eea90d041be9254898ac30aaec4001bf0b2a
eubash <nemezidaxxx@mail.ru>
2022-04-15 16:35:26 +0200

    CU-26b3tb4 - Fix refresh token downgrade

--- src/plugins/axiosInterceptor.ts
+++ src/plugins/axiosInterceptor.ts
@@ -5,11 +5,37 @@ import { useAuthStore } from '@/stores/auth';
 import { i18n } from '@/i18n';
 import { get } from '@/helpers/storage';
 import router from '@/router';
+import SentryUtil from '@/helpers/sentryUtil';
 
 import { EStorageKeys } from '@/types/storage';
 import { Route } from '@/router/types';
 
-//TODO: what API calls should be authorized
+/*
+ * Multiple parallel API calls handling
+ */
+let _isRefreshing = false;
+let _refreshQueue = [] as Array<(token: string) => void>;
+
+const _subscribeTokenRefresh = (cb: (token: string) => void) => {
+  _refreshQueue.push(cb);
+};
+
+const _onRefreshed = (token: string) => {
+  _refreshQueue.map((cb) => cb(token));
+};
+
+const _refreshToken = async (token: string): Promise<string> => {
+  const authStore = useAuthStore();
+  if (await authStore.verifyToken()) {
+    await authStore.refresh();
+    return (await get(EStorageKeys.token)) as string;
+  }
+  return token;
+};
+
+/*
+ * Not protected routes list
+ */
 const _notAuthorizedRoutes = (): string[] => {
   return [
     ...Object.values(apiService.auth).map((item) => item()),
@@ -30,19 +56,15 @@ const _requestHandler = async (
    */
   try {
     if (config.url && !_notAuthorizedRoutes().includes(config.url)) {
-      let token = await get(EStorageKeys.token);
+      const token = await get(EStorageKeys.token);
 
-      if (token) {
-        const authStore = useAuthStore();
-        if (await authStore.verifyToken()) {
-          await authStore.refresh();
-          token = await get(EStorageKeys.token);
-        }
-        config.headers['Authorization'] = `Bearer ${token}`;
+      if (token && !_isRefreshing) {
+        const newToken = await _refreshToken(token);
+        config.headers['Authorization'] = `Bearer ${newToken}`;
       }
     }
   } catch (error) {
-    console.log('return config erorr', error);
+    console.log('return config error', error);
   }
   return config;
 };
@@ -60,15 +82,43 @@ export default function init(): void {
       return response;
     },
     async (error) => {
-      if (error.response.status === 401) {
-        //TODO: define scenarios
-        //TODO: clear only token data
-        const authStore = useAuthStore();
-        /*
-         * Clear only expired token and refresh token
-         */
-        await authStore.clearTokenData();
-        router.push({ name: Route.WelcomeLogoScreen });
+      const {
+        config,
+        response: { status },
+      } = error;
+      if (config && status && status === 401) {
+        // console.log('STATUS', status);
+        if (!_isRefreshing) {
+          _isRefreshing = true;
+          const token = await get(EStorageKeys.token);
+          if (token) {
+            const newToken = await _refreshToken(token);
+            _isRefreshing = false;
+            _onRefreshed(newToken);
+            _refreshQueue = [];
+          } else {
+            SentryUtil.capture(
+              error,
+              'refresh token',
+              'async parallel calls',
+              'token absent',
+              { config }
+            );
+            router.push({ name: Route.WelcomeLogoScreen });
+          }
+        }
+        return new Promise((resolve) => {
+          _subscribeTokenRefresh((token) => {
+            config.headers.Authorization = `Bearer ${token}`;
+            resolve(axios(config));
+          });
+        });
+        // const authStore = useAuthStore();
+        // /*
+        //  * Clear only expired token and refresh token
+        //  */
+        // await authStore.clearTokenData();
+        // router.push({ name: Route.WelcomeLogoScreen });
       }
       //Logger error
       return Promise.reject(error);
