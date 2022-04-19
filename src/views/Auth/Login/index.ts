eee95599de772fbfefe2d1bcdb6924a1522d2890
AleksStek <AleksStek@yandex.ru>
2022-04-19 17:29:40 +0700

    Remove unused login page

--- src/views/Auth/Login/index.ts
+++ src/views/Auth/Login/index.ts
@@ -1,4 +1,3 @@
 export { default as Login1Step } from './Login1Step.vue';
 export { default as Login2Step } from './Login2Step.vue';
 export { default as Login3Step } from './Login3Step.vue';
-export { default as Login4Step } from './Login4Step.vue';
