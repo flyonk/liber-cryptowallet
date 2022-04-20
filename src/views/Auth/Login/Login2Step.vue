ee2ea4507df55002f5d30ad10f458725d1e55a78
shutemov <shutemov1997@mail.ru>
2022-04-20 19:07:11 +0300

    Adds TTopNavigation in Login2Step

--- src/views/Auth/Login/Login2Step.vue
+++ src/views/Auth/Login/Login2Step.vue
@@ -1,11 +1,19 @@
 <template>
-  <VerifyCode v-bind="$attrs" :flow="VerifyCodeFlow.Login" />
+  <t-top-navigation @click:left-icon="$emit('prev')">
+    <template #title>{{ $t('common.codeInput') }}</template>
+    <template #content>
+      <VerifyCode v-bind="$attrs" :flow="VerifyCodeFlow.Login" />
+    </template>
+  </t-top-navigation>
 </template>
 
 <script lang="ts" setup>
 import VerifyCode from '@/components/ui/organisms/auth/VerifyCode.vue';
+import TTopNavigation from '@/components/templates/TTopNavigation.vue';
 
 import { VerifyCodeFlow } from '@/components/ui/organisms/auth/types';
+
+defineEmits(['prev']);
 </script>
 
 <style lang="scss">
