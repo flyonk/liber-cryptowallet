10d96e0b28441aec1336ae608214a057f48759f3
Tema <shutemov1997@mail.ru>
2022-04-21 18:04:37 +0300

    Moves TTopNavigation to ui/templates

---
+++ src/components/ui/templates/TTopNavigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="template-top-navigation">
+    <div class="top-navigation">
+      <top-navigation
+        :left-icon-name="leftIconName"
+        :without-title="navWithoutTitle"
+        :with-custom-top-left="navWithCustomTopLeft"
+        @click:left-icon="$emit('click:left-icon')"
+        ><slot name="title" />
+        <template #top-left><slot name="top-left" /></template>
+        <template #top-right><slot name="top-right" /></template>
+        <template #subtitle><slot name="subtitle" /></template
+      ></top-navigation>
+    </div>
+    <div class="content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopNavigation from '@/components/ui/molecules/TopNavigation.vue';
+
+defineProps({
+  leftIconName: {
+    type: String,
+    default: 'icon-app-navigation-back',
+  },
+
+  navWithoutTitle: {
+    type: Boolean,
+    default: false,
+  },
+
+  navWithCustomTopLeft: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+defineEmits(['click:left-icon']);
+</script>
+
+<style lang="scss" scoped>
+.template-top-navigation {
+  padding: 30px 16px 0;
+
+  > .content {
+    margin-top: 25px;
+  }
+}
+</style>
