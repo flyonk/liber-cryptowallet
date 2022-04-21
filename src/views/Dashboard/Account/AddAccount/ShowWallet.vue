b00ea0fc9674345a190b96547e8101feb3c9f07b
Marsel M <melisovm97@gmail.com>
2022-04-21 17:01:49 +0600

    add new add account views

---
+++ src/views/Dashboard/Account/AddAccount/ShowWallet.vue
@@ -0,0 +1,21 @@
+<template>
+  <top-navigation>
+    {{ coinCode.toUpperCase() }}
+  </top-navigation>
+
+  <account-details :coin-code="coinCode" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { useAccountStore } from '@/stores/account';
+
+import { AccountDetails, TopNavigation } from '@/components/ui';
+
+const accountStore = useAccountStore();
+
+const coinCode = computed(() => accountStore.getNewAccountParams.coin);
+</script>
+
+<style scoped></style>
