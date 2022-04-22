968363086198b0c04b798f4204cec3bc5da7e0f9
AleksStek <AleksStek@yandex.ru>
2022-04-22 09:38:52 +0700

    Add confirmation popup

--- src/views/ConfigureApp/ChangeAuthApp.vue
+++ src/views/ConfigureApp/ChangeAuthApp.vue
@@ -21,10 +21,36 @@
   </div>
 
   <div style="padding: 15px; padding-bottom: 50px">
-    <base-button block @click="$router.push({ name: Route.ConfigureApp })">{{
+    <base-button block @click="onContinue">{{
       $t('configureApp.changeAppCTA')
     }}</base-button>
   </div>
+
+  <base-toast v-model:visible="showPopup" :severity="'attention'">
+    <template #description>
+      <div class="popup-description">
+        <h1 class="title">{{ $t('configureApp.ChangeAppPopupTitle') }}</h1>
+        <p class="description">
+          {{ $t('configureApp.ChangeAppMessage') }}
+        </p>
+      </div>
+    </template>
+    <template #footer>
+      <div class="popup-footer">
+        <BaseButton class="btn mb-3" size="large" @click="onConfirm">
+          {{ $t('common.continueCta') }}
+        </BaseButton>
+        <BaseButton
+          class="btn"
+          size="large"
+          view="secondary"
+          @click="showPopup = false"
+        >
+          {{ $t('common.cancelCta') }}
+        </BaseButton>
+      </div>
+    </template>
+  </base-toast>
 </template>
 
 <script lang="ts">
@@ -34,8 +60,23 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { TopNavigation, BaseButton } from '@/components/ui';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import { TopNavigation, BaseButton, BaseToast } from '@/components/ui';
 import { Route } from '@/router/types';
+
+const router = useRouter();
+
+const showPopup = ref(false);
+
+const onContinue = () => {
+  showPopup.value = true;
+};
+
+const onConfirm = () => {
+  router.push({ name: Route.ConfigureApp });
+};
 </script>
 
 <style lang="scss" scoped>
@@ -73,5 +114,33 @@ import { Route } from '@/router/types';
   flex-grow: 1;
   align-items: center;
   justify-content: center;
+}
+
+.popup-description {
+  margin-bottom: 65px;
+
+  > .title {
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 34px;
+    text-align: center;
+    letter-spacing: -0.0026em;
+    color: $color-brand-550;
+    margin-bottom: 8px;
+  }
+
+  > .description {
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 22px;
+    text-align: center;
+    letter-spacing: -0.0043em;
+    color: $color-brand-2-300;
+  }
+}
+
+.popup-footer {
+  display: flex;
+  flex-direction: column;
 }
 </style>
