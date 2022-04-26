9916e065c1802bc2f9a6e863409a6b23a44c6861
AleksStek <AleksStek@yandex.ru>
2022-04-26 16:31:19 +0700

    add store for mfa

---
+++ src/stores/mfa.ts
@@ -0,0 +1,37 @@
+// Кейсы по уровням безопасности
+// Если есть биометрика показываем face / touch id ->
+
+//   1. Если не установлен 2FA -> экран с ОТП
+// 2. Если установлен 2FA -> экран с 2FA
+
+// Если есть pascode и нет 2FA ->
+
+//   Экран с passcode и OTP
+
+// Если есть pascode и 2FA ->
+
+//   Экран с passcode и 2FA
+
+import { defineStore } from 'pinia';
+
+interface IMfaState {
+  twofa: boolean;
+  passcode: boolean;
+  opt: boolean;
+}
+
+// === 2fa Store ===
+
+export const use2faStore = defineStore('mfa', {
+  state: (): IMfaState => ({
+    twofa: false,
+    passcode: false,
+    opt: false,
+  }),
+
+  getters: {
+    getState: (state) => state,
+  },
+
+  actions: {},
+});
