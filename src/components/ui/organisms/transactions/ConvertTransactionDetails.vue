8f8fb6f49c2c7f72a32d319cc3a9d04415907109
Tema <shutemov1997@mail.ru>
2022-04-26 18:05:59 +0300

    Adds TTopNavigation to TransactionDetails

--- src/components/ui/organisms/transactions/ConvertTransactionDetails.vue
+++ src/components/ui/organisms/transactions/ConvertTransactionDetails.vue
@@ -1,6 +1,6 @@
 <template name="TransactionDetails">
-  <div class="transaction-details">
-    <TopNavigation class="header" @click:left-icon="$router.back()">
+  <t-top-navigation @click:left-icon="$router.back()">
+    <template #title>
       <div class="sum">
         <div class="sum-title">
           {{ transaction.sum }}
@@ -8,66 +8,69 @@
             {{ mainCoin }}
           </span>
         </div>
+      </div></template
+    >
+    <template #right
+      ><TransactionIconWithStatus
+        :status="transaction.status"
+        img-path="convert"
+    /></template>
+    <template #content>
+      <div class="transaction-details">
+        <div class="header">
+          <h2 class="sendto">{{ transaction.detailedInfo }}</h2>
+          <p class="date">
+            {{ relativeDate }}
+          </p>
+        </div>
+        <ul class="main mb-5">
+          <li class="main-item">
+            <p class="name">
+              {{ $t('status.title') }}
+            </p>
+            <TransactionStatus :status="transaction.status" />
+          </li>
+          <li class="main-item">
+            <p class="name">
+              {{ $t('transactions.statement') }}
+            </p>
+            <base-button size="medium" view="flat">
+              {{ $t('transactions.download') }}
+            </base-button>
+          </li>
+
+          <li class="main-item">
+            <p class="name">
+              {{ $t('transactions.rate') }}
+            </p>
+            <p class="description">{{ feeRate }}</p>
+          </li>
+
+          <li class="main-item">
+            <p class="name">
+              {{ $t('transactions.transferFee') }}
+            </p>
+            <p class="description">- {{ transaction.fee }} {{ mainCoin }}</p>
+          </li>
+
+          <li class="main-item">
+            <p class="name">
+              {{
+                transaction.direction === EDirection.income
+                  ? $t('transactions.cost')
+                  : $t('transactions.bought')
+              }}
+            </p>
+            <p class="description">
+              {{ transaction.direction === EDirection.income ? '-' : '+' }}
+              {{ transaction.oppositeCoin.amount }}
+              {{ transaction.oppositeCoin.code }}
+            </p>
+          </li>
+        </ul>
       </div>
-      <template #right>
-        <TransactionIconWithStatus
-          :status="transaction.status"
-          img-path="convert"
-        />
-      </template>
-    </TopNavigation>
-    <div class="header">
-      <h2 class="sendto">{{ transaction.detailedInfo }}</h2>
-      <p class="date">
-        {{ relativeDate }}
-      </p>
-    </div>
-    <ul class="main mb-5">
-      <li class="main-item">
-        <p class="name">
-          {{ $t('status.title') }}
-        </p>
-        <TransactionStatus :status="transaction.status" />
-      </li>
-      <li class="main-item">
-        <p class="name">
-          {{ $t('transactions.statement') }}
-        </p>
-        <base-button size="medium" view="flat">
-          {{ $t('transactions.download') }}
-        </base-button>
-      </li>
-
-      <li class="main-item">
-        <p class="name">
-          {{ $t('transactions.rate') }}
-        </p>
-        <p class="description">{{ feeRate }}</p>
-      </li>
-
-      <li class="main-item">
-        <p class="name">
-          {{ $t('transactions.transferFee') }}
-        </p>
-        <p class="description">- {{ transaction.fee }} {{ mainCoin }}</p>
-      </li>
-
-      <li class="main-item">
-        <p class="name">
-          {{
-            transaction.direction === EDirection.income
-              ? $t('transactions.cost')
-              : $t('transactions.bought')
-          }}
-        </p>
-        <p class="description">
-          {{ transaction.direction === EDirection.income ? '-' : '+' }}
-          {{ transaction.oppositeCoin.amount }}
-          {{ transaction.oppositeCoin.code }}
-        </p>
-      </li>
-    </ul>
-  </div>
+    </template>
+  </t-top-navigation>
 </template>
 
 <script lang="ts" setup>
@@ -78,7 +81,7 @@ import { getRelativeDate } from '@/helpers/datetime';
 
 import {
   BaseButton,
-  TopNavigation,
+  TTopNavigation,
   TransactionIconWithStatus,
   TransactionStatus,
 } from '@/components/ui';
