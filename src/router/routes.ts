e1a0291cafde07b38a9ae1fc763a1a161e045b41
Tema <shutemov1997@mail.ru>
2022-04-27 10:55:53 +0300

    Adds mata default layout to stories routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -157,11 +157,17 @@ const routes: Array<RouteRecordRaw> = [
         path: 'verifying-identity',
         name: Route.DashboardVerifyingStory,
         component: DashboardVerifyingIdentityStory,
+        meta: {
+          layout: 'default',
+        },
       },
       {
         path: 'transfer-funds-to-treasury',
         name: Route.DashboardTransferFundsToTreasuryStory,
         component: DashboardTransferFundsToTreasuryStory,
+        meta: {
+          layout: 'default',
+        },
       },
     ],
   },
