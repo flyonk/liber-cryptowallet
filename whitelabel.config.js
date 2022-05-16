3351666f2a5895fa2dd7834aefb919db9d4574e3
eubash <nemezidaxxx@mail.ru>
2022-05-16 17:15:19 +0200

    CU-2xpk3mj - Whitelabel application during it's compilation

---
+++ whitelabel.config.js
@@ -0,0 +1,21 @@
+/*
+ * Here must be added whitelabel configuration
+ */
+module.exports = {
+  liber: {
+    brand: 'liber',
+    theme: '../src/assets/styles/common/liber',
+    backgroundURL: '',
+    logoURL: '/img/logo.82b9c7a5.png',
+    contactEmail: '',
+    storageUrl: '',
+  },
+  dark: {
+    brand: 'Dark',
+    theme: '../src/styles/whitelabel-variables/dark',
+    backgroundURL: '',
+    logoURL: '/img/logo.82b9c7a5.png',
+    contactEmail: '',
+    storageUrl: '',
+  },
+};
