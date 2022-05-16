501a8e80be0d9ac342a7de00b17436d7e7cb901b
Tema <shutemov1997@mail.ru>
2022-05-16 09:37:05 +0300

    Adds errorHandler in Login1Step

--- src/views/Auth/Login/Login1Step.vue
+++ src/views/Auth/Login/Login1Step.vue
@@ -43,6 +43,7 @@ import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { useAuthStore } from '@/stores/auth';
+import { useErrorsStore } from '@/stores/errors';
 
 import { Route } from '@/router/types';
 
@@ -51,6 +52,7 @@ import TTopNavigation from '@/components/ui/templates/TTopNavigation.vue';
 
 const router = useRouter();
 const authStore = useAuthStore();
+const errorsStore = useErrorsStore();
 
 const number = ref('');
 const updateKey = ref(0);
@@ -78,7 +80,16 @@ const handleSelectCountry = (dialCode: string) => {
 const nextStep = async (phone: string) => {
   authStore.setPhone(phone);
 
-  await authStore.setPhoneToStorage();
+  try {
+    await authStore.setPhoneToStorage();
+  } catch (error) {
+    errorsStore.handle(
+      error,
+      'Login1Step.vue',
+      'nextStep',
+      'Error setting the phone in the storage'
+    );
+  }
 
   authStore.setStep(1, 'login');
 };
