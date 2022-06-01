d739bdad2299e62ea01d219782b5877bc4a02348
Tema <shutemov1997@mail.ru>
2022-06-01 17:05:26 +0300

    Creates TCtaInfo for add phone and add email case

---
+++ src/components/ui/templates/TCtaInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="cta-info">
+    <div class="image">
+      <slot name="image" />
+    </div>
+    <div class="content">
+      <slot name="content" />
+    </div>
+    <div class="fixed-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.cta-info {
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-start;
+  height: 100vh;
+
+  > .image {
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+  }
+
+  > .content {
+    margin-top: 50px;
+  }
+
+  > .fixed-footer {
+    position: fixed;
+    bottom: 50px;
+  }
+}
+</style>
