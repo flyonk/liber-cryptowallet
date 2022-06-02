68ef8ce9980faa814053fa3218cd09b5d9168bf2
Tema <shutemov1997@mail.ru>
2022-06-02 15:59:52 +0300

    fix: removes logging 406 errors

--- src/components/ui/organisms/auth/VerifyCode.vue
+++ src/components/ui/organisms/auth/VerifyCode.vue
@@ -22,7 +22,6 @@ import { useAuthStore } from '@/stores/auth';
 import { useProfileStore } from '@/stores/profile';
 import { use2faStore } from '@/stores/2fa';
 import { useErrorsStore } from '@/stores/errors';
-import errorService from '@/services/errorService';
 import EnterVerificationCode from '@/components/ui/organisms/auth/EnterVerificationCode.vue';
 import { Route } from '@/router/types';
 import { EKYCStatus, EUserStatus } from '@/models/profile/profile';
@@ -158,12 +157,6 @@ const onComplete = async (data: string) => {
       _otp.value = otp;
       is2fa.value = true;
       verificationCode.value = '';
-      errorService.logError(
-        err,
-        'VerifyCode',
-        'onComplete',
-        'New device case use 2fa'
-      );
       return;
     }
 
