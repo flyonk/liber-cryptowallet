eca979dbd06a11284ea92b927dc2c75ff52adbcb
AleksStek <AleksStek@yandex.ru>
2022-06-16 16:25:28 +0700

    references: #CU-2zfrezx \nfeat: update vars for static and api

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Middleware file to pass constants and env variables to the App
 export const STORE_AUTH_KEY = 'liber_auth';
-export const BASE_API_URL = process.env.VUE_APP_BASE_URL;
+export const BASE_API_URL = process.env.VUE_APP_BASE_API_URL;
 export const API_VERSION = process.env.VUE_APP_API_VERSION;
 export const CAPACITOR_WEB_ERROR = 'UNIMPLEMENTED';
-export const STATIC_BASE_URL = process.env.VUE_APP_STATIC_BASE_URL;
+export const STATIC_BASE_URL = process.env.VUE_APP_STATIC_STORAGE_URL;
 // export const AUTH_API_URL = process.env.VUE_APP_AUTH_URL;
 // export const PROFILE_API_URL = process.env.VUE_APP_PROFILE_URL;
