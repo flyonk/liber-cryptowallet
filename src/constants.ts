31598613d1a667dce678a356e439587b61c35074
AleksStek <AleksStek@yandex.ru>
2022-06-17 04:16:43 +0700

    references: #CU-2zfrezx \nfeat: move env vars to contants

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,18 @@ export const CAPACITOR_WEB_ERROR = 'UNIMPLEMENTED';
 export const STATIC_BASE_URL = process.env.VUE_APP_STATIC_STORAGE_URL;
 // export const AUTH_API_URL = process.env.VUE_APP_AUTH_URL;
 // export const PROFILE_API_URL = process.env.VUE_APP_PROFILE_URL;
+export const COUPONS_ENABLED = !!process.env.VUE_APP_COUPONS_ENABLED;
+export const CRYPTO_CURRENCIES_ENABLED =
+  !!process.env.VUE_APP_CRYPTO_CURRENCIES_ENABLED;
+export const FIAT_ENABLED = !!process.env.VUE_APP_FIAT_ENABLED;
+export const MULTI_LANGUAGE = !!process.env.VUE_APP_MULTI_LANGUAGE;
+export const TRANSACTIONS_ENABLED =
+  !!process.env.VUE_APP_TRANSACTIONS_FOR_NOT_VERIFIED_USERS;
+export const SUPPORT_EMAIL = process.env.VUE_APP_SUPPORT_EMAIL;
+
+let _langs = process.env.VUE_APP_SUPPORTED_LANGUAGES;
+if (_langs) {
+  _langs = _langs.replaceAll(' ', '').split(',');
+}
+
+export const SUPPORTED_LANGUAGES = _langs || [];
