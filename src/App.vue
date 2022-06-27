796934220219cebcb63e1fb34d3aadaf60378f88
eubash <nemezidaxxx@mail.ru>
2022-06-27 14:51:22 +0200

    feat: add offline bundler usage references: #337z15u

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,13 @@
       </router-view>
     </div>
   </app-layout-switcher>
-  <a-offline-bundler :is-active="isActiveBundler" />
+  <a-offline-bundler :is-active="bundlerIsActive" />
   <errors-toast />
   <m-custom-error />
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { onBeforeMount, onBeforeUnmount, ref, computed } from 'vue';
 
 //TODO: use profile store instead
 import { useAccountStore } from '@/applications/liber/stores/account';
@@ -56,6 +56,26 @@ store.init();
 
 const errorsStore = useErrorsStore();
 
+const bundlerIsActive = ref(false);
+
+function onOffline() {
+  bundlerIsActive.value = true;
+}
+function onOnline() {
+  bundlerIsActive.value = false;
+}
+
+onBeforeMount(() => {
+  //Need to subscribe window events
+  window.addEventListener('offline', onOffline);
+  window.addEventListener('online', onOnline);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('offline', onOffline);
+  window.removeEventListener('online', onOnline);
+});
+
 SwipeBack.enable()
   .then()
   .catch((err) => {
@@ -75,10 +95,6 @@ const showMfa = computed(() => {
   return mfaStore.enabled;
 });
 
-const isActiveBundler = computed(() => {
-  return !navigator.onLine;
-});
-
 const isOfflineMode = ref(isOffline());
 
 function handleReconnection() {
@@ -93,9 +109,8 @@ function handleReconnection() {
   > .link {
     font-weight: bold;
     color: #2c3e50;
-    /* stylelint-disable*/
-    &.router-link-exact-active {
-      /* stylelint-enable*/
+
+    & > .router-link-exact-active {
       color: #42b983;
     }
   }
--- src/components/ui/atoms/AOfflineBundler.vue
+++ src/components/ui/atoms/AOfflineBundler.vue
@@ -3,7 +3,7 @@
     v-if="isActive"
     class="offline-bundler text--callout font-weight--medium"
   >
-    {{ $t('offline.bundleTitle') }}
+    <i class="icon-warning" /> {{ $t('offline.bundleTitle') }}
   </div>
 </template>
 
@@ -22,12 +22,15 @@ defineProps({
   color: $color-white;
   background-color: $color-red-500;
   position: absolute;
-  top: 0;
-  left: 0;
   height: max-content;
   padding: 15px;
-  text-align: center;
   font-size: 3vw;
   width: 100%;
+  display: flex;
+  justify-content: space-around;
+
+  > i {
+    font-size: 18px;
+  }
 }
 </style>
