060ad27ce794e73d5cc9ea3aeba5ea9c1d1d52e5
Nikita <nikitys@mail.ru>
2022-07-04 22:09:36 +0300

    feat: add confirm passcode screen in update passcode \nreferences: #CU-31u3f0h

--- src/views/ConfigureApp/ChangePasscodeEnter.vue
+++ src/views/ConfigureApp/ChangePasscodeEnter.vue
@@ -5,25 +5,54 @@
     @click:left-icon="router.push({ name: Route.ChangePasscode })"
   >
     <template #title>
-      {{ $t('views.passcodeEnter.newPasscode') }}
+      {{ title }}
     </template>
     <template #content>
       <div class="passcode">
-        <base-passcode :action-type="EPasscodeActions.update" />
+        <base-passcode
+          v-if="actionType === EPasscodeActions.store"
+          :action-type="actionType"
+          :show-touch-faceid="false"
+          @submit="onCreate"
+        />
+        <base-passcode
+          v-if="actionType === EPasscodeActions.update"
+          :action-type="EPasscodeActions.update"
+        />\
       </div>
     </template>
   </t-top-navigation>
 </template>
 
 <script lang="ts" setup>
+import { computed, Ref, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { EPasscodeActions } from '@/types/base-component';
 import { Route } from '@/router/types';
+import { useI18n } from 'vue-i18n';
 
 import { BasePasscode, TTopNavigation } from '@/components/ui';
 
 const router = useRouter();
+const actionType = ref(EPasscodeActions.store) as Ref<EPasscodeActions>;
+const { tm } = useI18n();
+
+const title = computed(() => {
+  switch (actionType.value) {
+    case EPasscodeActions.store:
+      return tm('views.passcodeEnter.newPasscode');
+    case EPasscodeActions.update:
+      return tm('views.passcodeEnter.confirmPasscode');
+    default:
+      return tm('views.passcodeEnter.createPasscode');
+  }
+});
+function onCreate(success: boolean): void {
+  if (success) {
+    actionType.value = EPasscodeActions.update;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
