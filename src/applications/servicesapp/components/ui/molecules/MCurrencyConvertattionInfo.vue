7a316449fb54fb95d93dceb65bf48375350daae0
AleksStek <AleksStek@yandex.ru>
2022-07-08 09:12:19 +0700

    feat: fix styles references: #CU-33detz6

--- src/applications/servicesapp/components/ui/molecules/MCurrencyConvertattionInfo.vue
+++ src/applications/servicesapp/components/ui/molecules/MCurrencyConvertattionInfo.vue
@@ -82,49 +82,6 @@ const rateTitle = computed(() => {
   width: 100%;
 }
 
-.fees-item {
-  display: flex;
-  margin-bottom: 10px;
-  margin-left: -10px;
-
-  > .circle {
-    background: #eaefff;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 20px;
-    height: 20px;
-    margin-right: 10px;
-    padding-bottom: 2px;
-    font-weight: bold;
-    font-size: 14px;
-  }
-
-  > .sum {
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 16px;
-    display: flex;
-    align-items: center;
-    color: $color-brand-550;
-    margin-right: 8px;
-  }
-
-  > .name {
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-    display: flex;
-    align-items: center;
-    color: $color-brand-2-300;
-
-    &.-red {
-      color: $color-red-500;
-    }
-  }
-}
-
 .middle-info {
   display: flex;
   justify-content: space-between;
