9f23211ec489e8cb37daff28cf776974bde2bfdd
AleksStek <AleksStek@yandex.ru>
2022-07-13 23:44:56 +0700

    feat: libersave get crypto references: #CU-33detza

---
+++ src/applications/servicesapp/views/GetCrypto/GetCrypto.vue
@@ -0,0 +1,34 @@
+<template name="send-to">
+  <t-top-navigation
+    @click:left-icon="$router.push({ name: ServicesRoutes.GetCouponsEmail })"
+  >
+    <template #title>{{ $t('services.convert.title') }}</template>
+    <template #content>
+      <div class="send-to">
+        <div class="sendto-main">
+          <o-change-currency />
+        </div>
+      </div>
+    </template>
+  </t-top-navigation>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<script setup lang="ts">
+import OChangeCurrency from '@/applications/servicesapp/components/ui/organisms/transfers/OChangeCurrency.vue';
+import { ServicesRoutes } from '@/applications/servicesapp/router/types';
+import { TTopNavigation } from '@/components/ui';
+</script>
+
+<style lang="scss" scoped>
+.send-to {
+  height: 100vh;
+  flex-grow: 1;
+  overflow: auto;
+}
+</style>
