b6b2bea0b906120f61617215938169dc5114d430
AleksStek <AleksStek@yandex.ru>
2022-07-29 10:28:10 +0700

    feat: get contacts by ContactMapper references: #CU-3380a2e

--- src/helpers/contacts/contacts.ts
+++ src/helpers/contacts/contacts.ts
@@ -18,17 +18,31 @@ function ContactException(message: string, code: string) {
 
 export class ContactsPluginWeb implements ContactsPlugin {
   async getPermissions(): Promise<PermissionStatus> {
-    throw new (ContactException as any)(
-      'getPermissions - Not implemented on web.',
-      UNIMPLEMENTED_CODE_ERROR
-    );
+    const supported = 'contacts' in navigator && 'ContactsManager' in window;
+    if (!supported) {
+      throw new (ContactException as any)(
+        'getPermissions - Not implemented on web.',
+        UNIMPLEMENTED_CODE_ERROR
+      );
+    }
+    return {
+      granted: true,
+    };
   }
 
   async getContacts(): Promise<{ contacts: Contact[] }> {
-    throw new (ContactException as any)(
-      'getContacts - Not implemented on web.',
-      UNIMPLEMENTED_CODE_ERROR
-    );
+    const props = ['name', 'email', 'tel', 'address', 'icon'];
+    const opts = { multiple: true };
+    try {
+      const _navigator: any = window.navigator;
+      const contacts = await _navigator.contacts.select(props, opts);
+      return contacts;
+    } catch (ex) {
+      throw new (ContactException as any)(
+        'getContacts - Not implemented on web.',
+        UNIMPLEMENTED_CODE_ERROR
+      );
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
