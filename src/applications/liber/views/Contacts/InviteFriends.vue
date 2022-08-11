e2b0eb6d8e310c6bb94b5ad00f08eeee50db3f7e
Petr Flyonk <flyonk@Petrs-MacBook-Pro.local>
2022-08-11 22:20:05 +0400

    feat: contacts list markup, fixes, i18n for first invite friends page. references: #39g7uar

--- src/applications/liber/views/Contacts/InviteFriends.vue
+++ src/applications/liber/views/Contacts/InviteFriends.vue
@@ -1,7 +1,7 @@
 <template name="invite-friends">
   <t-top-navigation
     leftIconName="icon-app-navigation-close"
-    class="min-h-screen"
+    class="max-h-screen overflow-hidden"
     @click:left-icon="$router.go(-1)"
   >
     <template #title>{{ $t('views.inviteFriends.title') }}</template>
@@ -10,22 +10,26 @@
         <p class="text--body mb-3">
           {{ $t('views.inviteFriends.inviteFriend') }}
         </p>
-        <div class="controls flex w-full h-full">
-          <m-base-button size="large" class="mr-2 w-1/2"
+        <div
+          class="controls flex w-full h-full"
+          :class="{ 'mb-5': !isContactsListAllowed }"
+        >
+          <m-base-button icon-left="icon-link" size="medium" class="grow mr-2"
             ><p class="text--footnote whitespace-nowrap">
-              <i class="icon-link" /> {{ $t('views.inviteFriends.shareLink') }}
+              {{ $t('views.inviteFriends.shareLink') }}
             </p></m-base-button
           >
           <m-base-button
+            icon-left="icon-check"
+            size="medium"
+            class="grow"
             @click="$router.push({ name: Route.ContactsSendInvite })"
-            size="large"
-            class="w-1/2"
             ><p class="text--footnote whitespace-nowrap">
               {{ $t('views.inviteFriends.invitePhone') }}
             </p></m-base-button
           >
         </div>
-        <div class="main" v-if="isContactsListAllowed">
+        <div v-if="isContactsListAllowed" class="main">
           <img
             :src="`${STATIC_BASE_URL}/static/illustrations/empty_friend.png`"
             alt="friend"
@@ -41,8 +45,21 @@
             {{ $t('views.inviteFriends.acessContacts') }}
           </p>
         </div>
-        <div class="main" v-else>
-          <contacts-list :contacts="contacts" />
+        <div v-else class="main w-full h-full">
+          <h2 class="text--footnote color-dark-gray font-medium mb-1">
+            {{ $t('views.inviteFriends.contactsList.contacts') }}
+          </h2>
+          <p class="text--footnote mb-4">
+            {{ $t('views.inviteFriends.contactsList.privacy') }}
+          </p>
+          <m-base-input v-model="filterContacts" type="email">
+            <template #label>
+              {{
+                $t('views.inviteFriends.contactsList.searchFilter')
+              }}</template
+            >
+          </m-base-input>
+          <contacts-list :contacts="mockedContacts" />
         </div>
       </div>
     </template>
@@ -52,24 +69,22 @@
 <script setup lang="ts">
 import { inject, ref } from 'vue';
 import { Route } from '@/router/types';
-import { useRoute } from 'vue-router';
+// import { useRoute } from 'vue-router';
 // import { useAccountStore } from '@/applications/liber/stores/account';
 import { STATIC_BASE_URL } from '@/constants';
 import { TTopNavigation } from '@/components/ui';
 import { uiKitKey } from '@/types/symbols';
 import ContactsList from '@/components/ui/organisms/ContactsList.vue';
-import { useRecipientsStore } from '@/stores/recipients';
-import { Contact } from '@/types/contacts';
+// import { useRecipientsStore } from '@/stores/recipients';
+// import { Contact } from '@/types/contacts';
+import { mockedContacts } from '@/helpers/contacts';
 
-const { meta } = useRoute();
 const uiKit = inject(uiKitKey);
-const { MBaseButton } = uiKit!;
-const recipientsStore = useRecipientsStore();
+const { MBaseButton, MBaseInput } = uiKit!;
+// const recipientsStore = useRecipientsStore();
 
 const isContactsListAllowed = ref(false);
-const contacts: Contact[] = meta?.friends
-  ? recipientsStore.getFriends
-  : recipientsStore.getContacts;
+// const contacts: Contact[] = recipientsStore.getContacts;
 </script>
 
 <style lang="scss" scoped>
@@ -84,8 +99,8 @@ const contacts: Contact[] = meta?.friends
 
   > .main {
     display: flex;
+    height: 400px;
     flex-direction: column;
-    align-items: center;
 
     > .image {
       width: 155px;
@@ -100,5 +115,31 @@ const contacts: Contact[] = meta?.friends
       color: #2862ff;
     }
   }
+}
+
+:deep(.container) {
+  white-space: nowrap;
+}
+
+:deep(.base-input) {
+  width: 100%;
+  margin-bottom: 24px;
+}
+
+:deep(.main-list) {
+  max-height: 350px;
+  overflow: auto;
+}
+
+:deep(.base-button) {
+  min-width: 35% !important;
+
+  &:last-child {
+    min-width: 64% !important;
+  }
+}
+
+.template-top-navigation > :deep(.content) {
+  margin-top: 0;
 }
 </style>
