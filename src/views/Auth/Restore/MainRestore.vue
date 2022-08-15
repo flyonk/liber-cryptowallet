95aecdb18ca26e117b87604e0de0b6be58a87ffa
Petr Flyonk <flyonk@Petrs-MacBook-Pro.local>
2022-08-15 14:20:59 +0400

    feat: temporary makryp fix for suspended screen before authpageswitcher refactor. references: #37pzxva

--- src/views/Auth/Restore/MainRestore.vue
+++ src/views/Auth/Restore/MainRestore.vue
@@ -2,11 +2,15 @@
   <router-view />
   <!-- todo: temporary disabled  -->
   <!-- <auth-page-switcher :components="components" scope="recover" /> -->
-  <restore-contact-support />
+  <restore-contact-support v-if="route.path !== '/restore/suspended'" />
 </template>
 
 <script lang="ts" setup>
 import RestoreContactSupport from '@/views/Auth/Restore/RestoreContactSupport.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
 // todo: temporary disabled
 // import AuthPageSwitcher from '@/components/ui/organisms/auth/AuthPageSwitcher.vue';
 // import {
