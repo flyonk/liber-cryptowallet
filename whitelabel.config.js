931141630dade6a215a0ac98a0097ff07246629d
Tema <shutemov1997@mail.ru>
2022-08-16 17:38:51 +0300

    fix: updates whitelabel.config references: #000

--- whitelabel.config.js
+++ whitelabel.config.js
@@ -11,7 +11,7 @@ module.exports = {
     storageUrl: '',
     nameApp: 'Liber App',
     appIconsFolder: 'liber',
-    version: '0.1.47',
+    version: '0.1.50',
     mainColor: '#2862ff',
   },
   dark: {
