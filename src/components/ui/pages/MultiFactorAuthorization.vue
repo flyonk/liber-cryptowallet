c2c4edd291084bbddd27a4b8cfb798604a5325cc
AleksStek <AleksStek@yandex.ru>
2022-08-31 11:47:32 +0700

    fix: hide mfa on history back references: #CU-2zj7732

--- src/components/ui/pages/MultiFactorAuthorization.vue
+++ src/components/ui/pages/MultiFactorAuthorization.vue
@@ -69,6 +69,7 @@ const pStore = useProfileStore();
 const { tm } = useI18n();
 
 // hook to change history for back action possible
+router.push({ hash: '#mfa' });
 
 const oneTimeCode = ref('');
 const passcode = ref('');
